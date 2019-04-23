ff78cc479cd1ac4555dee5ba652aacc448365312
Nick Anderson <nickanderson@gmail.com>
2019-04-23 09:04:22 -0700

    .

--- src/Helpers/consoleLogStyle.js
+++ src/Helpers/consoleLogStyle.js
@@ -1,6 +1,25 @@
 export const logColor = (...value) => {
+  let color
+  switch (Math.floor(Math.random() * 4)) {
+    case 0:
+      color = 'pink'
+      break
+    case 1:
+      color = 'orange'
+      break
+    case 2:
+      color = 'yellow'
+      break
+    case 3:
+      color = 'turquoise'
+      break
+    default:
+      color = 'pink'
+      break
+  }
+
   console.log(
     `%c ${value}`,
-    'color:pink;font-size:20px;font-weight:bold;font-family:Baskerville;background: #222;padding: 3px 20px;'
+    `color:${color};font-size:20px;font-weight:bold;font-family:Baskerville;background: #222;padding: 3px 20px;`
   )
 }
