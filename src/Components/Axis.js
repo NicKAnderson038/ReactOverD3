7d5628ef8546db731f6edf26dfd1fc9cd368993d
Nick Anderson <nickanderson@gmail.com>
2019-04-23 08:37:07 -0700

    clean up

--- src/Components/Axis.js
+++ src/Components/Axis.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { logColor } from '../Helpers/ConsoleLogStyle'
+import { logColor } from '../Helpers/consoleLogStyle'
 
 function getTicks(count, max) {
   return [...Array(count).keys()].map(d => {
